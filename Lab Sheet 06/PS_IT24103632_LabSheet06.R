3c97bf0ef385af355abf865fa2ac799fce2a06ec
IT24103632 <it24103632@my.sliit.lk>
2025-09-02 20:09:30 +0530

    Add files via upload

---
+++ Lab Sheet 06/PS_IT24103632_LabSheet06.R
@@ -0,0 +1,16 @@
+setwd("C:\\Users\\IT24103632\\Desktop\\LabSheet06")
+
+#Exercise 
+# Part 1
+# Q1
+# Binominal Distribution
+# X=the number of students who passed the test
+
+# Part 1
+# Q2
+1- pbinom(46,50,0.85,lower.tail = FALSE)
+
+# Part 3
+# Q3
+dpois(15,12)
+
